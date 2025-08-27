a8cdf24e5435f8738d9a9ee9f60046ffa23b85d9
Izhar <izharramdan93@gmail.com>
2025-08-27 09:52:34 +0700

    update about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div className="p-4 text-center">
       <h1 className="text-2xl font-bold">About Page</h1>
-      <p>This is a simple React technical test app 🚀</p>
+      <p>WIDE</p>
     </div>
   );
 }

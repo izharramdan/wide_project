dd5fa3ba9b20044d05358cff68a860a88e558cab
Izhar <izharramdan93@gmail.com>
2025-08-27 13:03:16 +0700

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import WideLogo from "./assets/wide-logo.svg";
 export default function App() {
   return (
     <Router>
-      <nav className="fixed top-0 left-0 right-0 z-50 p-4 bg-gray-800 text-white flex items-center gap-6 shadow-md">
+      <nav className="fixed top-0 left-0 right-0 z-50 p-4 bg-gray-200 text-black flex items-center gap-6 shadow-md">
         <img
           src={WideLogo}
           alt="Wide Logo"
-          className="h-8 w-auto mr-4 drop-shadow"
+          className="h-10 w-auto mr-4 drop-shadow"
         />
         <Link to="/" className="hover:underline">Home</Link>
         <Link to="/about" className="hover:underline">About</Link>
